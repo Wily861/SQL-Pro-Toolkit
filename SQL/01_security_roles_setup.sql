fbf09bc00e0a9b40e94ac0525b3e695d24d65edc
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 18:46:37 -0500

    Update 01_security_roles_setup.sql

--- SQL/01_security_roles_setup.sql
+++ SQL/01_security_roles_setup.sql
@@ -1,1 +1,17 @@
+-- Otorga privilegios SELECT sobre TABLAS y VISTAS del esquema PRDN
+SELECT
+   'GRANT SELECT ON ' || owner || '.' || object_name || ' TO "SERV_APP";' AS grant_stmt
+FROM
+   dba_objects
+WHERE
+   owner = 'PRDN'
+   AND object_type IN ('TABLE', 'VIEW');
 
+-- Otorga privilegios EXECUTE sobre PROCEDIMIENTOS del esquema PRDN
+SELECT
+   'GRANT EXECUTE ON ' || owner || '.' || object_name || ' TO "SERV_APP";' AS grant_stmt
+FROM
+   dba_objects
+WHERE
+   owner = 'PRDN'
+   AND object_type = 'PROCEDURE';
