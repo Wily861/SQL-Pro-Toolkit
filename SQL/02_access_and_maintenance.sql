f4339e31c7cd07bf8941a724f8b3ef3ace72f98c
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 18:47:23 -0500

    Update 02_access_and_maintenance.sql

--- SQL/02_access_and_maintenance.sql
+++ SQL/02_access_and_maintenance.sql
@@ -1,1 +1,14 @@
+-- 🧑‍💻 Crear usuario Leadpasivos
+CREATE USER Leadpasivos WITH PASSWORD 'h8A2WuDS';
 
+-- 🔑 Permitir conexión a la base de datos "dbOzono"
+GRANT CONNECT ON DATABASE "dbOzono" TO "usrDivisasRead";
+
+-- 📦 Otorgar uso sobre el esquema flujodivisasgiros
+GRANT USAGE ON SCHEMA flujodivisasgiros TO "usrDivisasRead";
+
+-- 👁️ Otorgar permiso de lectura sobre todas las tablas del esquema
+GRANT SELECT ON ALL TABLES IN SCHEMA "flujodivisasgiros" TO "usrDivisasRead";
+
+-- 🧹 Eliminar función ya no requerida
+DROP FUNCTION IF EXISTS flujos_cdt.registrotipo1(VARCHAR);
